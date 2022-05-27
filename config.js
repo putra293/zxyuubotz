3aa0da8903609924b800efb3c016fca619d780eb
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-05-27 15:46:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ global.eror = '「❗」Server LAGI ERROR COK!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© jarotbotz' //change the watermark 
 global.image = 'https://telegra.ph/file/052ca138a613c8859756f.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/c3bcc6f8e8574d1c5274f.jpg'
+global.thumbfoto = 'https://telegra.ph/file/16fd3099fec2f93201556.jpg'
 
 // image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
