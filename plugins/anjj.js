509e5956f24f9e53d988375c4a7d47fb5361f87a
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-05-27 14:19:09 +0700

    Update anjj.js

--- plugins/anjj.js
+++ plugins/anjj.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let caption = `*anjir toxic🗿 awokawokaowk truskan bakat mu*`
 
-conn.sendButton( m.chat, caption, `©️ zifabotz`, `anjay slibaw`, `.👍`, m)
+conn.sendButton( m.chat, caption, `©️ jarotbotz`, `anjay slibaw`, `.👍`, m)
 
        }
        
