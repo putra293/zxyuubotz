40d014750448bd4da97dd8a6b43939a5f4a9d393
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-05-28 23:14:30 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -71,7 +71,7 @@ handler.all = async function (m, { isBlocked }) {
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`oscarbotz Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} |zifabotz by.jarot`).catch(_ => _)
+        await this.setStatus(`oscarbotz Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} |Jarotbotz by.jarot`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
