9ce3bee8355e941f5553140229e9921cceb1a1b2
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-30 13:26:36 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -5,31 +5,30 @@ let handler = async (m, { conn }) => {
 
 let str = `››╭─〘 *Donasi* 〙
 ╭╡📮: Donasi ngab jgan gunain doang.
-│┝‷✧ *Pulsa:* 6285850539404
-│┝‷✧ *Dana:* 6285850539404
-│┝‷✧ *Gopay:* *_GADA_*
+│┝‷✧ *Pulsa:* 081224804625
+│┝‷✧ *TELKOMSEL:* 
 │╰───···─────
-│⁺◛˖ Ingin Donasi? Chat nomor dibawah
+│ Ingin Donasi? Chat nomor dibawah
 ╰──────────···───╮`
 let name = await conn.getName(m.sender)
 
 let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
 
   sumberImg = await (await fetch(fla + 'donasi')).buffer()
-  image = (await conn.prepareMessage('6285826114932@s.whatsapp.net', sumberImg, MessageType.image, { thumbnail: Buffer.alloc(0) })).message.imageMessage
+  image = (await conn.prepareMessage('6281224804625@s.whatsapp.net', sumberImg, MessageType.image, { thumbnail: Buffer.alloc(0) })).message.imageMessage
   res = await conn.prepareMessageFromContent(m.chat, {
     "productMessage": {
       "product": {
         "productImage": image,
         "productId": "4938174216214248",
-        "title": "DONASI JAROTBOTZ",
+        "title": "DONASI ZXYUUBOTZ",
         "description": '\n' + wm + '\n' + str,
         "retailerId": "Thanks For Donate !",
         "url": "▌│█║▌║▌║║▌║▌║█│▌",
         "descriptionCount": "999999999",
         "productImageCount": "1",
       },
-      "businessOwnerJid": "6285850539404@s.whatsapp.net",
+      "businessOwnerJid": "6281224804625@s.whatsapp.net",
       "contextInfo": {
         "forwardingScore": 9999,
         "isForwarded": false
