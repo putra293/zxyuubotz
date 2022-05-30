99d4a5bb8e9ff363571b34bbe8b77eebe35141bf
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-30 14:57:00 +0700

    Update botutama.js

--- plugins/botutama.js
+++ plugins/botutama.js
@@ -6,10 +6,10 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 
     "displayName": "OWNER",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:OWNER;;;\nFN:OWNER\nitem1.TEL;waid=6285850539404:6285850539404\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:jarotganteng559@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Amrozi888\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ JAROTBOTZ ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:OWNER;;;\nFN:OWNER\nitem1.TEL;waid=6281224804625:6281224804625\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:romlisuhanda0401.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Romzz\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ ZxyuuBotz ]───────\nEND:VCARD"
   }, {
     "displayName": "BOT UTAMA",
-      "vcard": ""BEGIN:VCARD\nVERSION:3.0\nN:bot utama;;;\nFN:BOT\nitem1.TEL;waid=6283865196475:62853865196475\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:roziganteng559@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Amrozi888\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ JAROTBOTZ ]───────\nEND:VCARD"
+      "vcard": ""BEGIN:VCARD\nVERSION:3.0\nN:bot utama;;;\nFN:BOT\nitem1.TEL;waid=6281224804625:6281224804625\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:myromlisuhanda0401@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Romzz\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ ZxyuuBotz ]───────\nEND:VCARD"
     }]
   }, MessageType.contactsArray, { quoted: m })
 }
