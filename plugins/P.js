c11ca55cda2cc216b8010e67ec33a8a981d0ce93
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-30 19:14:18 +0700

    Update P.js

--- plugins/P.js
+++ plugins/P.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 let handler = async (m) => {
-let stc = fs.readFileSync('./sticker/P.webp')
+let stc = fs.readFileSync('./src/P.webp')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Salam Dong Nggak Usah P, Kek Orng ga sekolah ae lu ngntd!!', 'status@broadcast')
 }
 
