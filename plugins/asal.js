54d8777b30b48945e9a70bd5932de369b41b1652
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-30 14:09:46 +0700

    Update asal.js

--- plugins/asal.js
+++ plugins/asal.js
@@ -3,24 +3,24 @@ let handler = async (m, { conn, text }) => {
   let ext= `
 ╭═════════════════
 ║╭──❉ *BIODATA OWNER* ❉───
-║│➸ *Nama* : jarot ( nama samaran ) 
-║│➸ *Umur* : 18
-║│➸ *Kelas* :  lulus
-║│➸ *Status* : butuh ayng AWOKAWOK
+║│➸ *Nama* : Romli Suhanda 
+║│➸ *Umur* : 16
+║│➸ *Kelas* :  9 
+║│➸ *Status* : _*ULTRAMEN*_
 ║╰───────────────
 ╰════════════════
 ╭════════════════
 ║╭──❉ *ASAL KOTA* ❉───
-║│➸ *DAERAH* : *_KALIMANTAN BARAT =}} KUBURAYA_*
-║│➸ *ASAL KOTA* : *_PONTIANAK_*
+║│➸ *DAERAH* : *_CIPATIK, BANDUNG BARAT_*
+║│➸ *ASAL KOTA* : *_CIPATIK_*
 ║│➸ *ALAMAT RUMAH* : *[PRIVASI]*
-║│➸ *Gmail* : jarotganteng559@gmail.com
+║│
 ║╰────────────────
 ╰═════════════════
 _Oke udah itu aja terimakasih_
 
 `.trim()
-conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'ROZI')).buffer(), ext, 'zifabotz', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewazifa', m)
+conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Romz')).buffer(), ext, 'ZxyuuBotz', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewazifa', m)
 
 }
 handler.help = ['asal', 'kota']
