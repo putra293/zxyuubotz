70139a33cf7a92da9078d099277832b7739a5698
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-01 05:41:04 +0700

    Update P.js

--- plugins/P.js
+++ plugins/P.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./lib/tmp.webp')
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*PA PE PA PE, SALAM YANG BENER NGNTD!!', 'status@broadcast')
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*SALAM YANG BENER NGNTD!!', 'status@broadcast')
 }
 
 handler.customPrefix = /^(p|P)$/i
