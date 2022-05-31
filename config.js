ff92b73bf4b8b012a05342cd7272064a429e97be
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-31 07:20:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ global.APIKeys = { // APIKey nya disini
 global.packname = 'ROMLI'
 global.author = 'NAIWIN'
 
-global.wait = '_*BENTAR ANJG LAGI PROSES*_'
+global.wait = '_*BENTAR NGAB LAGI PROSES*_'
 global.eror = '「❗」_*LAGI EROR NGAP🗿*_'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© Zxyuu-Botz' //change the watermark 
