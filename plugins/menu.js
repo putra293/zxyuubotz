f2c139b87d49de9a3855bd7de0b29b0a7814c260
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-31 20:33:17 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -8,24 +8,24 @@ const chats = conn.chats.all()
 const groups = chats.filter(v => v.jid.endsWith('g.us'))
 const defaultMenu = {
   before: `
-║╭─────⟢ 〔 *Zxyuu Botz* 〕 ⟣──────
-║│➸Hai, %name!
-║│➸Tersisa *%limit Limit*
-║│➸Role *%role*
-║│➸Level *%level (%exp / %maxexp)*
-║│➸[%xp4levelup]
-║│➸%totalexp XP secara Total
-║╭──⟢〔 *TANGGAL* 〕 ⟣──────
-║│➸Hari: *%week %weton*
-║│➸Tanggal: *%date*
-║│➸WaktuIslam:*%dateIslamic*
-║│➸Waktu: *%time*
-║╭──⟢ 〔 *TIME* 〕 ⟣──────
-║│➸Uptime: *%uptime (%muptime)*
-║│➸Database: %rtotalreg dari %totalreg
-║│➸Memory Used : 
-║│➸${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-╰─────────⟢
+Hai *%name!*
+┏━━〔 Info %name 〕━ꕥ
+┃✾ Limit : *%limit* Limit
+┃✾ Role : *%role*
+┃✾ Level :
+┃✾ *%level (%exp / %maxexp)* [%xp4levelup]
+┃✾ %totalexp XP secara Total 
+┗━━━━━━━━ꕥ
+
+┏──『 *Today* 』──⬣
+│⬡  Tanggal: 
+│⬡  *%week %weton, %date*
+│⬡  Tanggal Islam:
+│⬡  *%dateIslamic*
+│⬡  Waktu: *%time*
+│⬡  Uptime: *%uptime (%muptime)*
+│⬡  Database: %rtotalreg dari %totalreg
+┗──────────⬣
 %readmore`.trimStart(),
   header: '┏━━ꕥ〔 %category 〕ꕥ━⬣ ',
   body: '  ┃ ⎙ %cmd %islimit %isPremium',
