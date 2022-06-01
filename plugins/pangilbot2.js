423dcdfcc0704ebb4d50496d28b264e2ec1886a2
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-01 10:58:45 +0700

    Update pangilbot2.js

--- plugins/pangilbot2.js
+++ plugins/pangilbot2.js
@@ -7,7 +7,7 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
   }
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', `BOT BY ROMLI:3)
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', `BOT BY ROMLI:3`)
 }
 handler.customPrefix = /^(zxyuu)$/i
 handler.command = new RegExp
