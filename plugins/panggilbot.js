8e2e2ef1691c317f1960bcd76d1884a5c90533a3
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-01 10:33:27 +0700

    Update panggilbot.js

--- plugins/panggilbot.js
+++ plugins/panggilbot.js
@@ -7,7 +7,7 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
   }
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', `*AOWKWKWK*` : 'tidak diketahui'}`)
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', `*AOWKWKWK*`:`NGNTD`)
 }
 handler.customPrefix = /^(nai)$/i
 handler.command = new RegExp
