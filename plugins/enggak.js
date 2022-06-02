91fa3e672d88592d890e33b48f014460b58bc858
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-02 10:19:30 +0700

    Update enggak.js

--- plugins/enggak.js
+++ plugins/enggak.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 let handler = async (m) => {
-let stc = fs.readFileSync('./sticker/Enggak.webp')
+let stc = fs.readFileSync('./tmp/tmp.webp')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*ENGGAK*', 'status@broadcast')
 }
 
